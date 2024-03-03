034928b1362fbd96dc73ef93c3ac5967f6df2892
lilybw <lilybwanscher@gmail.com>
2024-03-04 00:15:39 +0100

    Locating beans

--- src/main/java/gbw/melange/core/discovery/DiscoveryAgent.java
+++ src/main/java/gbw/melange/core/discovery/DiscoveryAgent.java
@@ -1,6 +1,8 @@
 package gbw.melange.core.discovery;
 
 import gbw.melange.common.annotations.Space;
+import gbw.melange.common.errors.ClassConfigurationIssue;
+import org.reflections.Reflections;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.*;
@@ -14,13 +16,15 @@ public class DiscoveryAgent<T> {
 
     private final Class<T> userMainClass;
     private Package supposedRootPackageOfUser;
-    private final Set<Package> locatedPackages = new HashSet<>();
+    private Set<Class<?>> spaces = new HashSet<>();
+    private AnnotationConfigApplicationContext programContext = new AnnotationConfigApplicationContext();
 
-    public static <T> DiscoveryAgent<T> run(Class<T> mainClassInstance){
+    public static <T> DiscoveryAgent<T> run(Class<T> mainClassInstance) throws ClassConfigurationIssue{
         DiscoveryAgent<T> instance = new DiscoveryAgent<>(mainClassInstance);
         instance.findRootPackage();
         instance.gatherUserSpaces();
 
+        instance.refresh();
         return instance;
     }
 
@@ -32,20 +36,33 @@ public class DiscoveryAgent<T> {
 
     }
 
-    public void gatherUserSpaces(){
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.scan(supposedRootPackageOfUser.getName());
-        context.refresh();
+    public void gatherUserSpaces() throws ClassConfigurationIssue {
+        String basePackage = supposedRootPackageOfUser.getName();
+        Reflections reflections = new Reflections(basePackage);
 
-        List<String> spaces = List.of(context.getBeanNamesForAnnotation(Space.class));
-        for(String space : spaces){
-            System.out.println("[DA] Found Space: " + space);
+        Set<Class<?>> spaces = reflections.getTypesAnnotatedWith(Space.class);
+        System.out.println("[DA].gatherUserSpaces() found: " + spaces.stream().map(Class::toString).toList());
+
+        // Find all classes annotated with @Space
+        for (Class<?> spaceClass : spaces) {
+            String constructorErrMsg = BeanConstructorValidator.isValidClassForRegistration(spaceClass);
+            if(constructorErrMsg != null){
+                throw new ClassConfigurationIssue(constructorErrMsg);
+            }
+
+            programContext.registerBean(spaceClass);
         }
     }
+
+    private void refresh(){
+        programContext.refresh();
+    }
+
 
     private DiscoveryAgent(Class<T> userMainClass){
         assert userMainClass != null;
         this.userMainClass = userMainClass;
+
     }
 
 
--- src/main/java/gbw/melange/welcomeapp/discovery/CanItFindThis.java
+++ src/main/java/gbw/melange/welcomeapp/discovery/CanItFindThis.java
@@ -1,7 +1,29 @@
 package gbw.melange.welcomeapp.discovery;
 
 import gbw.melange.common.annotations.Space;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Space
 public class CanItFindThis {
+
+    //This should cause a problem
+    public CanItFindThis(int hi){}
+
+    public CanItFindThis(){}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

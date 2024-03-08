ede89bbca6035ad7b231415fff06adbd8e8f47f2
lilybw <lilybwanscher@gmail.com>
2024-03-08 08:15:33 +0100

    math
    And its wrong too! Somehow

--- src/main/java/gbw/melange/welcomeapp/HomeScreen.java
+++ src/main/java/gbw/melange/welcomeapp/HomeScreen.java
@@ -1,22 +1,26 @@
 package gbw.melange.welcomeapp;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 import gbw.melange.common.MeshTable;
 import gbw.melange.common.annotations.View;
 import gbw.melange.common.elementary.IElement;
 import gbw.melange.common.elementary.space.IScreenSpace;
 import gbw.melange.common.elementary.space.ISpaceProvider;
 import gbw.melange.common.gl_wrappers.GLDrawStyle;
+import gbw.melange.common.hooks.OnRender;
 import gbw.melange.elements.ComputedTransforms;
 import gbw.melange.shading.FragmentShader;
 import gbw.melange.shading.templating.gradients.GradientFragmentShaderBuilder;
 import gbw.melange.welcomeapp.processors.IHomeScreen;
+import org.lwjgl.system.MathUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 @View( layer = View.HOME_SCREEN )
-public class HomeScreen implements IHomeScreen {
+public class HomeScreen implements IHomeScreen, OnRender {
     private static final Logger log = LoggerFactory.getLogger(HomeScreen.class);
+    private IElement element;
     @Autowired
     public HomeScreen(ISpaceProvider<IScreenSpace> provider) {
         FragmentShader fragmentShader = new GradientFragmentShaderBuilder()
@@ -26,7 +30,7 @@ public class HomeScreen implements IHomeScreen {
 
         IScreenSpace space = provider.getScreenSpace(this);
 
-        IElement element = space.createElement()
+        element = space.createElement()
                 .setMesh(MeshTable.CIRCLE_64.getMesh()) //TODO: Introduce rotation. Only thing users are allowed to set
                 .styling()
                     .setBackgroundColor(fragmentShader)
@@ -40,5 +44,14 @@ public class HomeScreen implements IHomeScreen {
 
         //TODO: Hella illegal moves
         ((ComputedTransforms) element.computed()).getMatrix().setToScaling(.9f, .9f, .9f);
+    }
+    private double acc = 0;
+    @Override
+    public void onRender(double deltaT) {
+        acc += deltaT / 1000;
+        ((ComputedTransforms) element.computed()).getMatrix()
+                .setToScaling(MathUtils.sin((float) acc), MathUtils.cos((float) acc), .9f);
+        ((ComputedTransforms) element.computed()).getMatrix()
+                .setTranslation((float) acc, (float) acc, 0);
     }
 }

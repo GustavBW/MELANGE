39c39fe8f036ed8c88937290b58f915edd2f70fc
lilybw <lilybwanscher@gmail.com>
2024-03-16 18:11:04 +0100

    log4f config

--- src/main/java/gbw/melange/common/Log4jConfigFactory.java
+++ src/main/java/gbw/melange/common/Log4jConfigFactory.java
@@ -22,18 +22,35 @@ public class Log4jConfigFactory extends ConfigurationFactory {
     static Configuration createConfiguration(final String name, ConfigurationBuilder<BuiltConfiguration> builder) {
         builder.setConfigurationName(name);
         builder.setStatusLevel(Level.ERROR);
+
+        builder.add(builder.newLogger("gbw.melange", Level.INFO)
+                .add(builder.newAppenderRef("Stdout"))
+                .addAttribute("additivity", false));
+
+        builder.add(builder.newLogger("org.apache", Level.WARN)
+                .addAttribute("additivity", false));
+
+        builder.add(builder.newLogger("org.reflections", Level.WARN)
+                .addAttribute("additivity", false));
+
         builder.add(builder.newFilter("ThresholdFilter", Filter.Result.ACCEPT, Filter.Result.NEUTRAL).
-                addAttribute("level", Level.DEBUG));
+                addAttribute("level", Level.INFO));
+
         AppenderComponentBuilder appenderBuilder = builder.newAppender("Stdout", "CONSOLE").
                 addAttribute("target", ConsoleAppender.Target.SYSTEM_OUT);
+
         appenderBuilder.add(builder.newLayout("PatternLayout").
-                addAttribute("pattern", "%d [%t] %-5level: %msg%n%throwable"));
+                addAttribute("pattern", "%d{dd-MM HH:mm:ss.SSS} [%t] %highlight{%-5level} %c{-2} - %msg%n"));
+
         appenderBuilder.add(builder.newFilter("MarkerFilter", Filter.Result.DENY,
                 Filter.Result.NEUTRAL).addAttribute("marker", "FLOW"));
+
         builder.add(appenderBuilder);
+
         builder.add(builder.newLogger("org.apache.logging.log4j", Level.DEBUG).
                 add(builder.newAppenderRef("Stdout")).
                 addAttribute("additivity", false));
+
         builder.add(builder.newRootLogger(Level.ERROR).add(builder.newAppenderRef("Stdout")));
         return builder.build();
     }

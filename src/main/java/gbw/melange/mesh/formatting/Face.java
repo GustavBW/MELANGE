89a3d6da8e88b7c88e7ca8375b600bbbd4bf32de
lilybw <lilybwanscher@gmail.com>
2024-04-03 10:40:29 +0200

    melange-mesh extracted

--- src/main/java/gbw/melange/mesh/formatting/Face.java
+++
@@ -1,4 +0,0 @@
-package gbw.melange.mesh.formatting;
-
-public record Face(IRefAccVec3 v0, IRefAccVec3 v1, IRefAccVec3 v2) {
-}
--- src/main/java/gbw/melange/mesh/formatting/IRefAccVec4.java
+++
@@ -1,6 +0,0 @@
-package gbw.melange.mesh.formatting;
-
-public interface IRefAccVec4 extends IRefAccVec3 {
-    float w();
-    void w(float value);
-}

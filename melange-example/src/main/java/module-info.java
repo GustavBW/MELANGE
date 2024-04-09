a3359255ed21efd95b5b888d03ecd00705bf9577
lilybw <lilybwanscher@gmail.com>
2024-04-09 17:14:03 +0200

    migration part 9

--- melange-example/src/main/java/module-info.java
+++ melange-example/src/main/java/module-info.java
@@ -8,6 +8,7 @@ module melange.example {
     requires org.apache.logging.log4j;
     requires spring.beans;
     requires gdx;
+    requires gdx.jnigen.loader;
 
     opens gbw.melange.example to spring.beans;
 }

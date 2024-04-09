2536ad7aa4d0b54b00c18f173cc8c6622e72666b
lilybw <lilybwanscher@gmail.com>
2024-04-10 00:29:25 +0200

    ITS ALIVE (migration XI)
    Through relocating the jnigen sharedlibraryloader into gdx using mavens shade plugin from the user side (melange-example), it actually ran.
    Currently known bugs:
    The distributed configurations are not recieved by common (JPMS)
    Logging is down for the moment. It seems log4j can't find system.out anymore.
    Only terminal build and exec works (mvn install from root or from ./melange-example)

---
+++ melange-common/src/main/java/gbw/melange/common/fallback/NoopMeshPipelineConfig.java
@@ -0,0 +1,6 @@
+package gbw.melange.common.fallback;
+
+import gbw.melange.common.mesh.services.IMeshPipelineConfig;
+
+public class NoopMeshPipelineConfig implements IMeshPipelineConfig {
+}
---
+++ melange-common/src/main/java/gbw/melange/common/fallback/NoopShadingPipelineConfig.java
@@ -0,0 +1,48 @@
+package gbw.melange.common.fallback;
+
+import gbw.melange.common.shading.services.IShadingPipelineConfig;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NoopShadingPipelineConfig implements IShadingPipelineConfig {
+    @Override
+    public IShadingPipelineConfig setLoggingAspects(Set<LogLevel> level) {
+        return this;
+    }
+
+    @Override
+    public Set<LogLevel> getLoggingAspects() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public boolean getUseCaching() {
+        return false;
+    }
+
+    @Override
+    public IShadingPipelineConfig useCaching(boolean yesNo) {
+        return this;
+    }
+
+    @Override
+    public IShadingPipelineConfig clearGeneratedContentOnExit(boolean yesNo) {
+        return this;
+    }
+
+    @Override
+    public boolean getClearGeneratedOnExit() {
+        return false;
+    }
+
+    @Override
+    public IShadingPipelineConfig clearGeneratedOnStart(boolean yesNo) {
+        return this;
+    }
+
+    @Override
+    public boolean getClearGeneratedOnStart() {
+        return false;
+    }
+}
